12d841f7fb8aa55522dd4bc6b2737a8b4dfe7194
parvsehgal <parvsehgal2004@gmail.com>
2023-11-27 21:05:44 +0530

    implemented login functionality

--- controller/auth.js
+++ controller/auth.js
@@ -1,12 +1,12 @@
 const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 exports.signUpHandler = async (req, res) => {
   try {
     const { user, email, password, role } = req.body;
-
     const existingUser = await User.findOne({ user });
-
     if (existingUser) {
       return res.status(400).json({ msg: "user already exist" });
     }
@@ -17,10 +17,42 @@ exports.signUpHandler = async (req, res) => {
     } catch (err) {
       res.status(500).json({ msg: "faild to hash password" });
     }
-
     const newUser = await User.create({ user, email, password, role });
     return res.status(200).json(newUser);
   } catch (err) {
     res.status(500).json({ msg: "SignUp failed" });
+  }
+};
+
+exports.loginHandler = async (req, res) => {
+  const { email, password } = req.body;
+
+  let ourUser = await User.findOne({ email });
+
+  if (!ourUser) {
+    return res.status(500).json({ msg: "user does not exist" });
+  }
+  ourUser = ourUser.toObject();
+  let isMatch = await bcrypt.compare(password, ourUser.password);
+  if (isMatch) {
+    const payload = {
+      email: ourUser.email,
+      id: ourUser._id,
+    };
+
+    const ourToken = jwt.sign(payload, process.env.SECRET, { expiresIn: "2h" });
+    const ops = {
+      expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+      httpOnly: true,
+    };
+
+    res.cookie("ourCookie", ourToken, ops).status(200).json({
+      ourUser,
+      ourToken,
+      msg: "logged in sucessfully",
+    });
+  } else {
+    console.log(isMatch);
+    res.status(500).json("incorrect password");
   }
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const { signUpHandler } = require("../controller/auth");
+const { signUpHandler, loginHandler } = require("../controller/auth");
 
 router.post("/signup", signUpHandler);
+router.post("/login", loginHandler);
 
 module.exports = router;
